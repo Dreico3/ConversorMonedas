84331dcdc5dbaff911a8141112947495a690adc4
Erick Cruz <fis9.81@gmail.com>
2022-12-27 18:07:08 -0400

    conversor de monedas termionado

--- src/main/java/com/mycompany/conversormonedas/ConversorMonedas.java
+++ src/main/java/com/mycompany/conversormonedas/ConversorMonedas.java
@@ -8,6 +8,16 @@ import javax.swing.*;
  */
 public class ConversorMonedas{
     public static void main(String[] args) {
-        String opciones = (JOptionPane.showInputDialog(null,"seleccione una opcion de conversion", "menu",JOptionPane.QUESTION_MESSAGE,null,new Object[] {"bola","orela"},"seleccion")).toString();
+        String opciones = (JOptionPane.showInputDialog(null,"seleccione una opcion de conversion", "menu",JOptionPane.QUESTION_MESSAGE,null,new Object[] {"conversor monedas","conversor temperatura"},"selecciovn")).toString();
+        Conversor com= new Conversor();
+        Funcion monedas = new Funcion();
+        switch (opciones) {
+            case "conversor monedas":
+                double input = Double.parseDouble(JOptionPane.showInputDialog("ingrese el valor"));
+                monedas.ConvertirMonedas(input);
+                break;
+            default:
+                throw new AssertionError();
+        }
     }
 }
